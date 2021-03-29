613d1e170f8d1daa2ef839cd378c583189a9fe9f
Aditya Chandavale <67214878+adityachandavale@users.noreply.github.com>
2021-03-29 15:07:32 +0530

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -19,10 +19,10 @@ class Twitter:
             #tweet_message = datajson['text']
             #print(tweet_message)
 
-    consumer_key= 'hNJlx21hY5TVvnWosoCsxZ9d1'
-    consumer_secret= 'SvoC6cRbemotiht0N6DNFWd0D4D8mNYgUMowHiSf2t8jjEUoE0'
-    access_token= '2889340878-7AQ7okdh6yKpAE7Q100u7qFTNO4EGOw1mCVlA5o'
-    access_token_secret= 'tSOMMGjasdmqGu3SfOVVpcaKJ6qIiVwcMrEq9LkzwcvLU'        
+    consumer_key= ''
+    consumer_secret= ''
+    access_token= ''
+    access_token_secret= ''        
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token,access_token_secret)
